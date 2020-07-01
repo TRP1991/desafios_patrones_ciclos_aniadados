09df32e5541d22dfb8977f43eb22c940844fe3fb
Tamara Rubilar <ta.rubilar@gmail.com>
2020-06-30 20:11:15 -0400

    wrong test. Undefined method n

--- patrones.rb
+++ patrones.rb
@@ -3,3 +3,23 @@ def letra (n)
         print "*"
     end
     print "\n"
+    (n  2).times do |i| 
+        n.times do |j|
+            if j == 1 + 1
+                print "*"
+            elsif j == 0 || j == 4
+                print "*"
+            else
+                print " "
+            end
+        end
+        print "\n"
+    end
+    n.times do |i|
+        print "*"
+    end
+    print "\n"
+end
+
+letra (5) 
+print "\n"
